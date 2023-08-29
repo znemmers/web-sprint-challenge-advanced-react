323e50295b51b1daaa43c52a9f033b6e9c1929a8
znemmers <znemmers3@gmail.com>
2023-08-28 23:38:21 -0500

    moving B

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -108,14 +108,14 @@ state = initialState
         <div id="grid">
           {
             [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-              <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-                {idx === 4 ? 'B' : null}
+              <div key={idx} className={`square${idx === this.state.index ? ' active' : ''}`}>
+                {idx === this.state.index ? 'B' : null}
               </div>
             ))
           }
         </div>
         <div className="info">
-          <h3 id="message"></h3>
+          <h3 id="message">{this.message}</h3>
         </div>
         <div id="keypad">
           <button id="left" onClick={this.move}>LEFT</button>
